f49f242df373eae396c1050559581125f45ff9bb
Ouiki <39866518+VioleGracex@users.noreply.github.com>
2025-03-13 20:49:59 +0500

    0.11
    -sign in
    -sign up  to russian

--- src/pages/OtherPage/NotFound.tsx
+++ src/pages/OtherPage/NotFound.tsx
@@ -6,14 +6,14 @@ export default function NotFound() {
   return (
     <>
       <PageMeta
-        title="React.js 404 Dashboard | TailAdmin - React.js Admin Dashboard Template"
-        description="This is React.js 404 Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
+        title="React.js 404 Панель управления | TailAdmin - Шаблон панели администратора на React.js"
+        description="Это страница React.js 404 Панели управления для TailAdmin - Шаблон панели администратора на React.js с Tailwind CSS"
       />
       <div className="relative flex flex-col items-center justify-center min-h-screen p-6 overflow-hidden z-1">
         <GridShape />
         <div className="mx-auto w-full max-w-[242px] text-center sm:max-w-[472px]">
           <h1 className="mb-8 font-bold text-gray-800 text-title-md dark:text-white/90 xl:text-title-2xl">
-            ERROR
+            ОШИБКА
           </h1>
 
           <img src="/images/error/404.svg" alt="404" className="dark:hidden" />
@@ -24,14 +24,14 @@ export default function NotFound() {
           />
 
           <p className="mt-10 mb-6 text-base text-gray-700 dark:text-gray-400 sm:text-lg">
-            We can’t seem to find the page you are looking for!
+            Мы не можем найти страницу, которую вы ищете!
           </p>
 
           <Link
             to="/"
             className="inline-flex items-center justify-center rounded-lg border border-gray-300 bg-white px-5 py-3.5 text-sm font-medium text-gray-700 shadow-theme-xs hover:bg-gray-50 hover:text-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] dark:hover:text-gray-200"
           >
-            Back to Home Page
+            Назад на главную страницу
           </Link>
         </div>
         {/* <!-- Footer --> */}
