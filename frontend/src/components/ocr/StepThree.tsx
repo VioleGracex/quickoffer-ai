67bf871ce27a2cfbc5cc6951af52cdf7440d196e
Ouiki <39866518+VioleGracex@users.noreply.github.com>
2025-03-16 17:18:53 +0500

    0.24

--- frontend/src/components/ocr/StepThree.tsx
+++ frontend/src/components/ocr/StepThree.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { FiDownload, FiArrowLeft, FiFileText } from "react-icons/fi";
 import { FaFilePdf, FaFileWord } from "react-icons/fa";
-import { Button } from '@mui/material';
+import { Button, IconButton } from '@mui/material';
 
 interface StepThreeProps {
   ocrResult: string;
@@ -24,29 +24,28 @@ const StepThree: React.FC<StepThreeProps> = ({ ocrResult, handleDownloadText, se
   };
 
   return (
-    <div className="max-w-4xl mx-auto space-y-6  dark:text-white">
-      {outputFormat === '.txt' ? (
-        <div className="bg-white p-4 rounded shadow dark:bg-gray-800 dark:text-gray-300">
-          <textarea
-            value={ocrResult}
-            readOnly
-            rows={10}
-            className="w-full p-2 border rounded dark:bg-gray-700 dark:border-gray-600 dark:text-gray-300"
-          />
-        </div>
-      ) : (
-        <div className="flex flex-col items-center  p-4  ">
+    <div className="max-w-2xl mx-auto space-y-6 dark:text-white">
+      <div className="bg-white p-4 rounded shadow dark:bg-gray-800 dark:text-gray-300">
+        <textarea
+          value={ocrResult}
+          readOnly
+          rows={10}
+          className="w-full p-2 border rounded dark:bg-gray-700 dark:border-gray-600 dark:text-gray-300"
+        />
+      </div>
+      <div className="flex flex-row text-center bg-black max-w-[250px] mx-auto items-center justify-between p-4 rounded shadow dark:bg-gray-800">
+        <div className="flex flex-col items-center justify-between w-full">
           {renderIcon()}
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={handleDownloadText}
-            startIcon={<FiDownload />}
-          >
-            Скачать файл
-          </Button>
+          <p className="mt-2 text-center text-gray-300">
+            OCR Result {outputFormat}
+          </p>
         </div>
-      )}
+        <div>
+          <IconButton color="primary" onClick={handleDownloadText}>
+            <FiDownload style={{ fontSize: "2rem" }} />
+          </IconButton>
+        </div>
+      </div>
       <div className="flex flex-col items-center space-y-6">
         <div>
           <Button
